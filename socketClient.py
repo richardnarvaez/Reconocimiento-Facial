e13dad76cbef30982f9a9616ec41032a0cbb8833
NRamosD <nixonramosdue@gmail.com>
2022-01-07 09:01:01 -0500

    Files uploaded

---
+++ socketClient.py
@@ -0,0 +1,19 @@
+import socketio
+sio = socketio.Client()
+
+@sio.event
+def connect():
+    print('connection established')
+
+@sio.event
+def sendEvent(data):
+    print('message received with ', data)
+    sio.emit('rv', {'response': data})
+
+@sio.event
+def disconnect():
+    print('disconnected from server')
+
+def conectarse():
+    sio.connect('http://localhost:3000/')
+    sendEvent("Hola usuario")
