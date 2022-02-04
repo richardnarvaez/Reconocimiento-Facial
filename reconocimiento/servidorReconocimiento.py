7564a2759c11ead60de8f0a2945c01dce4e3fadf
NixonR <nixonramosdue@gmail.com>
2022-02-04 13:11:52 -0500

    Proyecto configurado para reconocimiento e identificación

--- reconocimiento/servidorReconocimiento.py
+++ reconocimiento/servidorReconocimiento.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import socketClient
 
 dato = os.path.dirname(__file__)
 rutaimagenes = os.path.join(dato,'img')
@@ -16,6 +17,8 @@ cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
 
 faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
 
+socketClient.conectarse()
+
 while True:
 	ret,frame = cap.read()
 	if ret == False: break
@@ -30,30 +33,16 @@ while True:
 		result = face_recognizer.predict(rostro)
 
 		cv2.putText(frame,'{}'.format(result),(x,y-5),1,1.3,(255,255,0),1,cv2.LINE_AA)
-		'''
-		# EigenFaces
-		if result[1] < 5700:
-			cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
-			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
-		else:
-			cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
-			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
-		
-		# FisherFace
-		if result[1] < 500:
-			cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
-			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
-		else:
-			cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
-			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
-		'''
-		# LBPHFace
+
 		if result[1] < 70:
 			cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
 			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
+			socketClient.sendEvent("Persona detectada 😍")
 		else:
 			cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
 			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
+			socketClient.sendEvent("No es la persona registrada 😒")
+			
 		
 	cv2.imshow('frame',frame)
 	k = cv2.waitKey(1)
