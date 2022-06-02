4d60488abf781fbc6eed4daad05fc7a927b891c9
Richard B. Vinueza <richard.bvn997@gmail.com>
2022-06-02 08:02:37 -0500

    Change Willy Test add TEST

--- entrenamiento/test/test_suiteTest3_2.py
+++ entrenamiento/test/test_suiteTest3_2.py
@@ -10,39 +10,39 @@ socket = socketio.Client()
 def test_testCase1():
     cap = cv2.VideoCapture()
     faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
-#     socket.connect("http://localhost:3000/")
-#     socket.disconnect()
+    socket.connect("http://localhost:3000/")
+    socket.disconnect()
     
     assert recognizeFaceTest.recognize(70, cap, faceClassif, socket,"willy") == "cp1"
 
 def test_testCase2():
     cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
     faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
-#     socket.connect("http://localhost:3000/")
-#     socket.disconnect()
+    socket.connect("http://localhost:3000/")
+    socket.disconnect()
 
     assert recognizeFaceTest2.recognize(70, cap, faceClassif, socket,"willy") == "cp2"
 
 def test_testCase3():
     cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
     faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
-#     socket.connect("http://localhost:3000/")
-#     socket.disconnect()
+    socket.connect("http://localhost:3000/")
+    socket.disconnect()
 
     assert recognizeFaceTest2.recognize(60, cap, faceClassif, socket,"willy") == "cp3"
 
 def test_testCase4():
     cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
     faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
-#     socket.connect("http://localhost:3000/")
-#     socket.disconnect()
+    socket.connect("http://localhost:3000/")
+    socket.disconnect()
 
     assert recognizeFaceTest3.recognize(70, cap, faceClassif, socket,"willy") == "cp4"
 
 def test_testCase5():
     cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
     faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
-#     socket.connect("http://localhost:3000/")
-#     socket.disconnect()
+    socket.connect("http://localhost:3000/")
+    socket.disconnect()
 
     assert recognizeFaceTest3.recognize(60, cap, faceClassif, socket,"willy") == "cp5"
